5d03ba884ba58fc13f833e16bb5eec27a5166ac2
SomiliDas <somili.das12@gmail.com>
2025-06-29 12:50:06 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -5,12 +5,21 @@ const path = require("path")
 
 const dbConnection = require("./config/dbConnection")
 const cors = require('cors')
+const allowedOrigins = [
+  'https://flight-training-platform-frontend.onrender.com',
+  'http://localhost:5173'
+];
+
 app.use(cors({
-  origin: ['https://flight-training-platform-frontend.onrender.com',
-           'http://localhost:5173'],
-  
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true
-}))
+}));
 
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
