36a036006285ca0f7eb29a84b7a4e5ecd469b72e
SomiliDas <somili.das12@gmail.com>
2025-06-29 12:23:12 +0530

    Update UserProfilePage.jsx

--- frontend/vite-project/src/pages/UserProfilePage.jsx
+++ frontend/vite-project/src/pages/UserProfilePage.jsx
@@ -61,7 +61,7 @@ const UserProilePage = () => {
             <button className=' block ml-150 px-4 py-1 bg-blue-600 rounded-lg text-white font-semibold cursor-pointer hover:underline' onClick={()=>(navigate(`/users/edit/${studentId}`, {state : {ename : user.name, eemail : user.email}}))}>Edit Profile</button>
             <div className=' mt-5 flex justify-center items-center'>
                 <div className='h-[150px] w-[150px] rounded-full'>
-                    <img src={`http://localhost:8000/images/${user.profilePic}`} className='h-full w-full object-cover border-1 border-gray-300 rounded-full'/>
+                    <img src={`https://flight-training-platform-backend.onrender.com/images/${user.profilePic}`} className='h-full w-full object-cover border-1 border-gray-300 rounded-full'/>
                 </div>
             </div>
                 <p className='text-[45px] text-blue-950 font-extrabold mb-1'>{user.name}</p>
