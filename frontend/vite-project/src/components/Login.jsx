db03258748a104e03e92c1495b2bdc95ad91b9bd
SomiliDas <somili.das12@gmail.com>
2025-06-29 12:57:02 +0530

    Update Login.jsx

--- frontend/vite-project/src/components/Login.jsx
+++ frontend/vite-project/src/components/Login.jsx
@@ -10,7 +10,7 @@ function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault()
     try {
-       const response = await fetch('https://flight-training-platform-backend.onrender.com/users/login', {
+       const response = await fetch('https://flight-training-platform-backend-cors.onrender.com/users/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
